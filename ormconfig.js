b765499306c02a013799e6f1294b0767116210bb
Nhat Khanh <lednhatkhanh@gmail.com>
2017-12-27 21:47:29 +0700

    Load db config from dotenv

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
+
 var isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
 	type: "postgres",
-	host: "localhost",
-	port: 5432,
-	username: "postgres",
-	password: "Abc123@@",
-	database: "food-friend-dev",
+	host: process.env.DB_HOST,
+	port: parseInt(process.env.DB_PASSWORD, 10),
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 	entities: isProd ? [`build/connector/entities/*.js`] : [`src/connector/entities/*.ts`],
 	subscribers: isProd ? [`build/connector/subscribers/*.js`] : [`src/connector/subscribers/*.ts`],
 	migrations: [`src/migrations/*.ts`],
